bba22a383bcd4f74822ccf325bc42ee800ef85ed
0anajuliaf <ana.santos3092@etec.sp.gov.br>
2024-04-25 08:43:53 -0300

    button useState

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './SRC/Components/Title';
+import Main from './SRC/Components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
+      <Main/>
       
-      <StatusBar style="auto" />
+   
     </View>
   );
 }
--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { View, TextInput, Text } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Text, Button } from "react-native";
 
 
 export default function  Form(){
+
+const[height, setHeight] = useState(null)
+const[weight, setWeight] = useState(null)
+const[messegeImc, setMessegeImc] = useState("preencha o peso e a altura");
+const[imc, setImc] = useState(null)
+const[textButton, setTextButton] = useState("Calcular IMC");
+
     return(
         <View>
             <View>
@@ -15,6 +22,13 @@ export default function  Form(){
            <TextInput
            placeholder= "Ex. 80.365"
            keyboard= "numeric"/>
+
+           <Button
+         
+          title="nnnn"
+           />
+
+
            </View>
         </View>
     );
